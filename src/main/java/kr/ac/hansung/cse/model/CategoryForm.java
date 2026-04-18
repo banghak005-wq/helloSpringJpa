2ade5a9628955de88eeb498bf6893c95092141d5
banghak005-wq <banghak005@gmail.com>
2026-04-19 02:38:12 +0900

    feat: CategoryForm.java 완성, categoryForm.html,categoryList.html 완성

---
+++ src/main/java/kr/ac/hansung/cse/model/CategoryForm.java
@@ -0,0 +1,14 @@
+package kr.ac.hansung.cse.model;
+
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor // Spring MVC 폼 바인딩 필수: 기본 생성자가 있어야 합니다.
+public class CategoryForm {
+    @NotBlank(message = "카테고리 이름을 입력하세요")
+    @Size(max = 50, message = "50자 이내로 입력하세요")
+    private String name; 
+}
